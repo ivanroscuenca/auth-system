4b9e7098173ba8ad317e71e0582e852d473a3d99
ivanros <ivanros@protonmail.com>
2023-09-15 11:42:13 +0200

    second commit

--- create.php
+++ create.php
@@ -1,28 +1,56 @@
 <?php require "includes/header.php"; ?>
 <?php require "config.php";?>
+<?php 
+
+if(isset($_SESSION['username'])) {
+  
+
+      if($_POST['title'] == "" OR $_POST['body'] == "") {
+        echo "some inputs are empty";
+        
+      } else {
+
+        $email = $_POST['title'];
+        $body = $_POST['body'];
+        $username = $_SESSION['username'];
+
+        
+
+        $insert = $conn->prepare("INSERT INTO posts (title,body,username) 
+         VALUES (:title, :body, :username)");
+
+         $insert->execute([
+          ':title' => $title,
+          ':body' => $body,
+          ':username' => $username,
+         ]);
+
+      }
+
+    }
+
+  
+?>
 
 <main class="form-signin w-50 m-auto">
+
   <form method="POST" action="create.php">
-    <!-- <img class="mb-4 text-center" src="/docs/5.2/assets/brand/bootstrap-logo.svg" alt="" width="72" height="57"> -->
+    
     <h1 class="h3 mt-5 fw-normal text-center">Create Post</h1>
 
     <div class="form-floating">
-      <input name="email" type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
-      <label for="floatingInput">Email address</label>
+      <input name="title" type="text" class="form-control" id="floatingInput" placeholder="title">
+      <label for="floatingInput">title</label>
     </div>
 
-    <div class="form-floating">
-      <input name="username" type="text" class="form-control" id="floatingInput" placeholder="username">
-      <label for="floatingInput">Username</label>
+
+    <div class="form-floating mt-4">
+     <textarea rows="9" name="body" placeholder="body" class="form-control"></textarea>
+      <label for="floatingPassword">Body</label>
     </div>
 
-    <div class="form-floating">
-      <input name="password" type="password" class="form-control" id="floatingPassword" placeholder="Password">
-      <label for="floatingPassword">Password</label>
-    </div>
-
-    <button name="submit" class="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
-    <h6 class="mt-3">Aleardy have an account?  <a href="login.php">Login</a></h6>
+    <button name="submit" class="w-100 btn btn-lg btn-primary mt-5" type="submit">Create Post</button>
+  
 
   </form>
 </main>
